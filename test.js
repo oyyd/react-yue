9a47754c055b738b96e5d4ccfeb3d5e1e88c89c5
oyyd <oyydoibh@gmail.com>
2018-06-01 23:30:12 +0800

    test: update exit code

--- test.js
+++ test.js
@@ -3,24 +3,18 @@ const childProcess = require('child_process')
 // NOTE: Tests may not ends automatically in windows so we add timeout
 // and stop it mannually.
 function main() {
-  const child = childProcess.exec('npm run test')
+  const child = childProcess.exec('node_modules/.bin/jest')
 
   child.stderr.pipe(process.stderr)
   child.stdout.pipe(process.stdout)
 
-  let errorInfo = false
-
-  child.stderr.on('data', () => {
-    errorInfo = true
-  })
-
   setTimeout(() => {
     child.kill()
-    process.exit(errorInfo ? 1 : 0)
+    process.exit()
   }, 30 * 1000)
 
-  child.on('exit', () => {
-    process.exit(errorInfo ? 1 : 0)
+  child.on('exit', (code) => {
+    process.exit(code)
   })
 }
 
